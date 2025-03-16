89b79fe84b9e60ecb6cfedadb5c471a421d5b696
JellyfishSi <2960946S@student.gla.ac.uk>
2025-03-16 10:55:43 +0000

    TeachingRequirementDAO接口的实现

---
+++ dao/impl/FileTeachingRequirementDAO.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 文件实现的教学需求数据访问对象 (File-based Teaching Requirement DAO)
+ * 使用文件系统实现教学需求数据的持久化
+ */
+public class FileTeachingRequirementDAO implements TeachingRequirementDAO {
+
+}
